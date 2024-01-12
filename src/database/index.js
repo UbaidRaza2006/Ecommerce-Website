1dc84d4c78794495ffb6f9a99e67175f36c3f45b
ubaidraza2006 <ubaidmemon2006@gmail.com>
2024-01-12 22:50:48 +0500

    mongodb connected

---
+++ src/database/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const configOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+const connectToDb = async () => {
+    const connectionUrl = 'mongodb+srv://ubaidrazabawany13:jxeChP4sr0iZmuiu@cluster0.sii5yv4.mongodb.net/';
+
+    mongoose.connect(connectionUrl, configOptions).then(() => console.log("Ecommmerce Database connected Successfully!")).catch((err) => console.log(`Getting error from Db Connection ${err.message}`))
+}
+
+export default connectToDb;
